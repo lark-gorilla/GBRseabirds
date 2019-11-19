6424366e837ad71846b3f2950e4dfd0e612b4294
lark-gorilla <mark.gr.miller@gmail.com>
2019-11-19 14:22:24 +0000

    Include Neumann and Shephard Terns

--- tracking_compile.R
+++ tracking_compile.R
@@ -9,13 +9,14 @@
 #sp=rep('WTSH', nrow(congdon_wtsh)),
 #colony=congdon_wtsh$Colony,
 #trackID=congdon_wtsh$TrackID,
-#date=congdon_wtsh$Date,
-#time=congdon_wtsh$Time,
+#date=gsub('-', '/', substr(congdon_wtsh$DateTime, 1, 10)),
+#time=substr(congdon_wtsh$DateTime, 12, 19),
 #latitude=congdon_wtsh$Latitude,
 #longitude=congdon_wtsh$Longitude)
 #write.csv(master, 'C:/seabirds/sourced_data/tracking_data/tracking_master.csv', quote=F, row.names=F)
 
 library(sf)
+library(readxl)
 master<-read.csv('C:/seabirds/sourced_data/tracking_data/tracking_master.csv')
 
 # Add Congdon MABO and BRBO data
@@ -43,9 +44,30 @@ p2<-strsplit(paste(p1$geometry), ',')
 p2_out<-NULL
 for(i in 1:length(p2))
   {p2_out<-rbind(p2_out, data.frame(latitude=as.numeric(p2[[i]][2]),
-                                    longitude=substr(p2[[i]][1], 3, nchar(p2[[i]][1]))))}
+                                    longitude=as.numeric(substr(p2[[i]][1], 3, nchar(p2[[i]][1])))))}
 
 master<-rbind(master, data.frame(dataID='SURM1',sp='LENO', colony='Pelsaert',
                                  trackID=p1$layer, date=substr(p1$timestamp, 1,10),
                                  time=substr(p1$timestamp, 12,19),p2_out))
+
+# Neumann Sooty Terns
+p1<-NULL
+for( i in 2:7)# exclude 1 as 6 lines
+{p1<-rbind(p1, read_xlsx('C:/seabirds/sourced_data/tracking_data/raw/Neumann_Seychelles_SOTE.xlsx', sheet=i))}
+
+master<-rbind(master, data.frame(dataID='NEUM1',sp='SOTE', colony='Seychelles',
+                                 trackID=p1$Tag,date=gsub('-', '/', p1$Date),time=p1$Time_T,
+                                 latitude=p1$Y,longitude=p1$X))
+
+# Shephard Brown Noddy and Sooty Tern
+
+p1<-read_xlsx('C:/seabirds/sourced_data/tracking_data/raw/Shephard_WA_BRNO.xlsx', sheet=3)
+p1<-p1[1:12812,]
+p1$sp='BRNO'
+p1[which(p1$Species==2),]$sp<-'SOTE'
+p1$colony='Lancelin'
+p1[which(p1$Species==2),]$colony<-'Rat'
+master<-rbind(master, data.frame(dataID='SHEP1',sp=p1$sp, colony=p1$colony,
+                                 trackID=factor(p1$ID),date=gsub('-', '/', substr(p1$DateTimeWST, 1,10)),time=substr(p1$DateTimeWST, 12, 19),
+                                 latitude=p1$LatDD,longitude=p1$LongDD))
 
