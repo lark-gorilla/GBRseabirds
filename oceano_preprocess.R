9228c65f2c919b7c58539b136663b5bc82bf717e
lark-gorilla <mark.gr.miller@gmail.com>
2020-06-25 20:30:15 +0100

    quick kernel ouput loop created for error checking and editing

--- oceano_preprocess.R
+++ oceano_preprocess.R
@@ -386,6 +386,54 @@ print(k)
 } #end multi-sp loop
 
 
+# FAST kernel outputting to check errors
+
+sp_groups<-
+  list('BRBO', 'MABO', 'RFBO', 'WTSH', 'SOTE', 
+       c('GRFR', 'LEFR', 'MAFR'), c('RBTB', 'RTTB'), 
+       c('BRNO', 'LENO', 'BLNO'),c('CRTE', 'ROTE', 'CATE'))
+
+names(sp_groups) <- c('BRBO', 'MABO', 'RFBO', 'WTSH', 'SOTE',
+                      'FRBD', 'TRBD', 'NODD', 'TERN')
+
+hval<-0.03
+
+k = sp_groups[1]
+
+  dat<-master_embc[master_embc$sp %in% unlist(k),]
+  # collapse colz within species
+  colz_sp<-colz[colz$sp %in% unlist(k),]
+
+  for(i in unique(dat$coly))
+  {
+    b1<-dat[dat$coly==i,]
+    spdf<-SpatialPointsDataFrame(coords=b1[,c(7,8)],  data=data.frame(coly=b1$coly),
+                                 proj4string =CRS(projection(ex_templ)))
+    r1<-crop(ex_templ, (extent(spdf)+0.3))
+    r_pix<-as(r1,"SpatialPixels")
+ 
+    # Foraging within 50% UD
+    bfor<-b1[b1$embc=='foraging',]
+    spdf<-SpatialPointsDataFrame(coords=bfor[,c(7,8)],  data=data.frame(coly=bfor$coly),
+                                 proj4string =CRS(projection(ex_templ)))
+    
+    spdf$coly<-factor(spdf$coly)
+    
+    KDE.Surface <- kernelUD(spdf,same4all = F, h=hval, grid=r_pix)
+    KDE.50 <- getverticeshr(KDE.Surface, percent = 50)
+    #write_sf(st_as_sf(KDE.50), 'C:/seabirds/temp/brbo_for_50UD.shp')
+
+    # bind up polygons for export
+    if(which(i==unique(dat$coly))==1){all_kerns<-st_as_sf(KDE.50)}else{all_kerns<-rbind(all_kerns, st_as_sf(KDE.50))}
+    print(i)
+  }
+  
+  #plot(all_pts[all_pts$coly=='Swains' & all_pts$layer==0, 'weight'])
+  # export polygons for gis
+  write_sf(all_kerns, paste0('C:/seabirds/temp/', names(k), 'kernhull_temp.shp'), delete_dsn=T)
+
+
+
 # Create colony max range buffers and extract
 # Pull in trip quality table
 t_qual<-read.csv('C:/seabirds/data/tracking_trip_decisions.csv')
