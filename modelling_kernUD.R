0cb359a9449da9ab70c9efa291368a1b9f7ca061
lark-gorilla <mark.gr.miller@gmail.com>
2020-06-27 07:34:38 +0100

    Edits to loop code for all sp_groups

--- modelling_kernUD.R
+++ modelling_kernUD.R
@@ -9,106 +9,98 @@ library(caret)
 library(pROC)
 library(vegan)
 library(raster)
+library(ncf)
 
-#set.seed(7) really important to set.seed with RF??
+set.seed(24) # so that random samples and random forest models can be reproduced
 
 # read in data
-brbo<-read.csv('C:/seabirds/data/BRBO_forKern_modelready.csv')
+names(sp_groups) <- c('BRBO', 'MABO', 'RFBO', 'SOTE','WTST', 'WTLG',
+                      'FRBD', 'TRBD', 'NODD', 'TERN')
 
-hulls<-read.csv('C:/seabirds/data/BRBO_hulls_modelready.csv')
+for(k in sp_groups)
+
+dat<-read.csv(paste0('C:/seabirds/data/modelling/kernhull_pts/', k, '_kernhull.csv'))
+names(dat)[names(dat)=='layer']<-'forbin'
+dat$forbin<-as.factor(dat$forbin)
+dat[dat$forbin==1,]$weight=999 # change from NA for no.omit
+
+# RM sp name unless k has conflicts
+if(k!='FRBD'){dat$spcol<-substr(dat$spcol, 6, nchar(as.character(dat$spcol)))}
 
 # clean up data
-
-# Collapse ID to just remove owner and species, this combines all Soanes Dog
-# and Swains cols - check for differences in interval
-hulls$ID<-do.call(c, lapply(strsplit(as.character(hulls$ID), '_'), function(x)x[3]))
-hulls$ID<-substr(hulls$ID, 1, (nchar(hulls$ID)-4)) 
-
-# remove sitting behaviour and error pts
-
-brbo$forbin=1
-hulls$forbin=0
-
-brbo<-rbind(brbo, hulls)
-
-
 # set bathy vals >0 to NA then make positive
-brbo[brbo$ex_bathy>0,]$ex_bathy<-NA
-brbo$ex_bathy<-sqrt(brbo$ex_bathy^2)
-brbo<-na.omit(brbo)
+dat[dat$bth>0,]$bth<-NA
+dat$bth<-sqrt(dat$bth^2)
+dat<-na.omit(dat)
 
-brbo%>%group_by(ID)%>%summarise(nP=length(which(forbin==1)),nA=length(which(forbin==0)))
+dat%>%group_by(spcol)%>%summarise(nP=length(which(forbin==1)),nA=length(which(forbin==0)))
 
-brbo<-brbo%>%group_by(ID)%>%mutate(npres=length(which(forbin==1)))
+dat<-dat%>%group_by(spcol)%>%mutate(npres=length(which(forbin==1)))
 
-brbo<-rbind(brbo%>%filter(forbin==1), brbo%>%filter(forbin==0)%>%group_by(ID)%>%
-  sample_n(size=(unique(npres)*3), replace=T)) # 3:1 Pa to A
+dat<-rbind(dat%>%filter(forbin==1), dat%>%filter(forbin==0)%>%group_by(spcol)%>%
+  sample_n(size=(unique(npres)*3), replace=T, weight=weight)) # 3:1 Pa to A
 
-brbo$npres<-NULL
+dat$npres<-NULL
 
 # niche overlap
-enviro_std<-decostand(brbo[,c(2:11)], method="standardize")
+enviro_std<-decostand(dat[,c(4:13)], method="standardize")
 enviro_rda<-rda(enviro_std, scale=T)
 screeplot(enviro_rda)
 enviro.sites.scores<-as.data.frame(scores(enviro_rda, choices=1:4, display='sites', scaling=1))
-enviro.sites.scores<-data.frame(enviro.sites.scores,brbo[,c(1,12)])
+enviro.sites.scores<-data.frame(enviro.sites.scores,dat[,c(1,2)])
 ggplot(data=enviro.sites.scores, aes(x=PC1, y=PC2))+
-  geom_density_2d(aes(colour=factor(forbin)))+facet_wrap(~ID)+theme_bw()
-
+  geom_density_2d(aes(colour=factor(forbin)))+facet_wrap(~spcol)+theme_bw()
 
 # Remember nearshore front values which ==0 should be NA
 
 # scale: normalise (0-1) each covariate per dataID
 # Remember to check for outliers prior to normaliziation
 normalized<-function(x){(x-min(x))/(max(x)-min(x))}
-brbo_norm<-brbo %>% group_by(ID) %>% mutate_if(is.numeric, normalized)%>%as.data.frame()
+dat_norm<-dat %>% group_by(spcol) %>% mutate_if(is.numeric, normalized)%>%as.data.frame()
 
-for( i in unique(brbo_norm$ID))
+for( i in unique(dat_norm$spcol))
 {
-  #pr1<-table(brbo_norm[brbo_norm$ID==i,]$forbin)[1]/table(brbo_norm[brbo_norm$ID==i,]$forbin)[2]
-  cw<-ifelse(brbo_norm[brbo_norm$ID==i,]$forbin==0, 1, 3)  
+  #pr1<-table(dat_norm[dat_norm$spcol==i,]$forbin)[1]/table(dat_norm[dat_norm$ID==i,]$forbin)[2]
+  cw<-ifelse(dat_norm[dat_norm$spcol==i,]$forbin==0, 1, 3)  
   
-  rf1<-ranger(factor(forbin)~sst_mn+sst_sd+chl_mn+chl_sd+mfront_sd+pfront_mn+ex_bathy+ex_slope,
-              data=brbo_norm[brbo_norm$ID==i,], num.trees=500, mtry=3, importance='impurity',
+  rf1<-ranger(forbin~sst+sst_sd+chl+chl_sd+mfr_sd+pfr+bth+slp,
+              data=dat_norm[dat_norm$spcol==i,], num.trees=500, mtry=3, importance='impurity',
               probability =T)
   print(rf1)
   print(ranger::importance(rf1))
   # predict to other colonies
-  brbo_norm$p1<-predict(rf1, data=brbo_norm)$predictions[,2] # prob of foraging 0-1
-  names(brbo_norm)[which(names(brbo_norm)=='p1')]<-i
+  dat_norm$p1<-predict(rf1, data=dat_norm)$predictions[,2] # prob of foraging 0-1
+  names(dat_norm)[which(names(dat_norm)=='p1')]<-i
   # predict to GBR
   #gbr_norm$p1<-predict(rf1, newdata=gbr_norm, type='prob')[,2] # prob of foraging 0-1
   #names(gbr_norm)[which(names(gbr_norm)=='p1')]<-i
   
   # SPAC assessment
-  sp1<-spline.correlog(brbo_norm[brbo_norm$ID==i,]$Longitude,
-                       brbo_norm[brbo_norm$ID==i,]$latitude, residuals(rf1, type="pearson"),
-                       na.rm=T,latlon=T,resamp=10)
-  plot(sp1)
-  
-  
-  
+ # sp1<-spline.correlog(dat_norm[dat_norm$spcol==i,]$Longitude,
+#                       dat_norm[dat_norm$spcol==i,]$latitude, residuals(rf1, type="pearson"),
+#                       na.rm=T,latlon=T,resamp=100)
+#  plot(sp1)
   print(i)
   print(Sys.time())
 }
 
 # check cols selected and size of matrix before running
-br1<-as.data.frame(brbo_norm[,c(1, 12, 13:28)])%>%gather(variable, value, -ID, -forbin)
-aucz<-br1%>%group_by(ID, variable)%>%
+br1<-as.data.frame(dat_norm[,c(1, 2, 16:31)])%>%gather(variable, value, -spcol, -forbin)
+aucz<-br1%>%group_by(spcol, variable)%>%
   summarise(auc=pROC::roc(forbin, value,direction="<")$auc,
             thresh=coords(pROC::roc(forbin, value,direction="<"),0.5, transpose=F)$threshold,
             sens=coords(pROC::roc(forbin, value,direction="<"),0.5, transpose=F)$sensitivity,
             spec=coords(pROC::roc(forbin, value,direction="<"),0.5, transpose=F)$specificity)
 # handy
-qplot(data=filter(br1, ID=='Prickly Pear'& variable=='Danger'), x=value, fill=factor(forbin), geom='histogram')
+qplot(data=filter(br1, spcol=='Prickly Pear'& variable=='Danger'), x=value, fill=factor(forbin), geom='histogram')
 
 
-m1<-matrix(ncol=16, nrow=16, data =aucz$auc, dimnames=list(unique(aucz$ID), unique(aucz$ID)))
+m1<-matrix(ncol=16, nrow=16, data =aucz$auc, dimnames=list(unique(aucz$spcol), unique(aucz$spcol)))
 d1<-as.dist(1-m1)
 
 plot(hclust(d1, method='average'))
 
-ggplot(aucz, aes(x = ID, y = variable)) + 
+ggplot(aucz, aes(x = spcol, y = variable)) + 
   geom_raster(aes(fill=auc)) + 
   geom_text(aes(label=round(auc, 2)))+
   scale_fill_gradient(low="grey",  high="red") +
@@ -119,11 +111,11 @@ ggplot(aucz, aes(x = ID, y = variable)) +
 
 #caret approach
 
-#test_dat<-as.data.frame(brbo_norm[brbo_norm$ID %in% unique(brbo_norm$ID)[c(2,4,5)],])
+#test_dat<-as.data.frame(dat_norm[dat_norm$ID %in% unique(dat_norm$ID)[c(2,4,5)],])
 
 #try subsample data to something that will actually run
-brbo_norm%>%group_by(ID)%>%summarise(n())
-test_dat<-brbo_norm%>%group_by(ID)%>%
+dat_norm%>%group_by(ID)%>%summarise(n())
+test_dat<-dat_norm%>%group_by(ID)%>%
   slice(1:4500)%>%as.data.frame()  # grab first 4500 rows
 
 # test_dat has to be data.frame otherwise caret 'y' classerror
