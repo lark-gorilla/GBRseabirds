fe9b43e72889637a5a7d0db86d947394111d1b30
lark-gorilla <mark.gr.miller@gmail.com>
2021-02-02 20:30:35 +0000

    Edits to mk_gbrplot() function to change to paper fig

--- plots_maps.R
+++ plots_maps.R
@@ -473,7 +473,7 @@ col_rad_cores<-st_read('C:/seabirds/data/GIS/col_radii_core_hotspots_smooth_10pe
 #### ~~~~ GBR plot function ~~~~ ####
 mk_gbrplot<-function(spg='TERN_MultiCol'){
   ext<-unlist(raster::extract(subset(mod_pred, spg),
-  as(filter(rad_diss, md_spgr==substr(spg, 1, 4) & rd_clss=='max'), 'Spatial')))
+  as(filter(radz, md_spgr==substr(spg, 1, 4)), 'Spatial')))
   mn<-min(ext, na.rm=T)
   mx<-max(ext, na.rm=T)
   
@@ -485,35 +485,27 @@ mk_gbrplot<-function(spg='TERN_MultiCol'){
   col_sp<-substr(spg, 1, 4)
   if(col_sp=='WTLG'){col_sp<-'WTST'} 
   
-  sp.rad<-filter(rad_diss, md_spgr==substr(spg, 1, 4) &
-                   rd_clss %in% c('max', 'mea', 'min'))
-  #beeb<-st_bbox(sp.rad)
-  
-  xl<-c(142, 156)
-  yl<-c(-29, -7)
-  if(substr(spg, 1, 4) %in%c('WTLG', 'TRBD', 'FRBD', 'SOTE')){
-    xl<-c(142, 161)  
-    yl<-c(-29, -6)}
+  beeb<-st_bbox(filter(radz, md_spgr==substr(spg, 1, 4)))
 
-  p1<-ggplot() +
+  p_gbr<-ggplot() +
   layer_spatial(data=r1) +
   geom_sf(data=filter(colz, md_spgr==col_sp), shape = 23, fill = "yellow") +
-  geom_sf(data=sp.rad, aes(colour=rd_clss), fill='NA') +
   geom_sf(data=gbrmp, col='white', fill='NA') +
-  geom_sf(data=land, col='black', fill='grey') +
+    geom_sf(data=filter(confz,md_spgr==substr(spg, 1, 4)), colour='red', fill=NA)+
+    geom_sf(data=filter(obz,md_spgr==substr(spg, 1, 4)), colour='green', fill=NA)+
+    geom_sf(data=filter(radz, md_spgr==substr(spg, 1, 4)), colour='blue', fill=NA)+
   theme_bw()+
   annotation_scale(location = "bl")+  
   annotation_north_arrow(location = "tr", which_north = "true")+
   labs(x='Longitude', y='Latitude')+
-  coord_sf(xlim = xl, ylim =yl, expand = FALSE)+
-  scale_colour_manual('Forgaing radii', values=c('#00FFFF','#66FFCC', '#00FF66' ), labels=c(
-    'Maximum', 'Mean', 'Minimum'))+
+  coord_sf(xlim=beeb[c(1,3)],ylim=beeb[c(2,4)], expand = FALSE)+
+
     scale_fill_viridis_b('Likely\nseabird\nforaging\nhabitat', option='magma',
                          breaks=c(seq(0.1, 0.9, 0.1)),labels=c('low', rep('', 7), 'high'), na.value = NA)
   #scale_fill_viridis('Likely\nforaging\nhabitat', limits=c(mn, mx), breaks=c(mn, mx), labels=c('low', 'high'),
    #                  option='magma', na.value = NA)
 
-  return(p1)}
+  return(p_gbr)}
 
 #theme(legend.position = c(0.2, 0.3), legend.background = element_rect(fill = "grey"),
  #     legend.key = element_rect(fill = "grey"))
@@ -555,8 +547,13 @@ dev.off()
 #### ~~~~ AUC-core areas plot (paper fig) ~~~~ ####
 
 # read in auc-calced core areas
-corez<-st_read('C:/seabirds/data/GIS/col_radii_auc_core_smooth.shp')
+corez<-st_read('C:/seabirds/data/GIS/col_radii_auc_core_smooth_holecrumb100.shp')
 
+#corez2<-corez%>%drop_crumbs(threshold=100000000)%>% 
+#              fill_holes(threshold=100000000)# drop <100km2 crumbs and holes
+#write_out
+#st_write(corez2, 'C:/seabirds/data/GIS/col_radii_auc_core_smooth_holecrumb100.shp')  
+                      
 ggplot(data=corez, aes(x=auc, y=area_km2, colour=md_spgr))+geom_point()+
   geom_line(aes(group=interaction(md_spgr, site_nm)))
 
@@ -689,32 +686,23 @@ corez$site_auc_type<-as.character(corez$auc_type)
 corez[paste(corez$md_spgr, corez$site_nm, corez$auc) %in%
         paste(lookup1$md_spgr, lookup1$site_nm, lookup1$auc),]$site_auc_type<-'first'
 corez$site_auc_type<-ifelse(corez$auc_type=='obs', 'obs', corez$site_auc_type)
+# and order sp
+corez$md_spgr<-factor(corez$md_spgr, levels=c("BRBO", 'MABO', 'RFBO', 'WTST',"WTLG",'FRBD', 'TRBD', "SOTE" , 'NODD', 'TERN'))
+
 
 radz<-filter(corez, auc==0.5 )
 obz<-filter(corez, site_auc_type=='obs' )
 confz<-corez%>%group_by(md_spgr, site_nm)%>%filter(site_auc_type!='sim')%>%
   filter(row_number()==n())%>%ungroup()
 
-# plots
-p_base<-ggplot() +
-  geom_sf(data=land, col='black', fill='grey') +
-  #geom_sf(data=filter(gbr_reef, FEAT_NAME=='Reef'), fill='NA', colour=alpha('black',0.5))+
-  geom_sf(data=gbrmp, col='black', fill='NA', linetype='dashed') +
-  geom_sf(data=colz%>%group_by(site_nm)%>%filter(species==first(species)), shape = 23, fill = "yellow")+
-  theme_bw()+
-  annotation_scale(location = "bl")+  
-  annotation_north_arrow(location = "tr", which_north = "true")+
-  labs(x='Longitude', y='Latitude')
-
 # merge layers for better vis
 # merge colz by md_spgr and rd_class for gbr-wide plots
-rad_diss<-radz%>%group_by(md_spgr)%>%summarize(geometry = st_union(geometry))
-# and dsgntn_n? maybe not
-# use test1<-st_simplify(test1, dTolerance=0.01)
-obs_diss<-obz%>%group_by(md_spgr)%>%summarize(geometry = st_union(geometry))
+rad_diss<-radz%>%group_by(md_spgr, dsgntn_n)%>%summarize(geometry = st_union(geometry))
 
-conf_diss<-confz%>%filter(site_auc_type!='sim')%>%
-  filter(row_number()==n())%>%ungroup()%>%group_by(md_spgr)%>%summarize(geometry = st_union(geometry))
+obs_diss<-obz%>%group_by(md_spgr, dsgntn_n)%>%summarize(geometry = st_union(geometry))
+
+conf_diss<-confz%>%st_buffer(0)%>%filter(site_auc_type=='first')%>%
+  group_by(md_spgr, dsgntn_n)%>%summarize(geometry = st_union(geometry))
 
 p_rad<-p_base+
   geom_sf(data=filter(corez, auc==0.5 ), aes(colour=spcol), fill=NA)+
